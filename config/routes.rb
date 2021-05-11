dd51fe5abc5d6d9012f53c75ffff64ec3f5e4ee8
HollyHilts <holly.a.hilts@gmail.com>
2021-05-11 15:28:03 -0400

    finished all routes and CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: :new_task
   get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   get '/tasks/:id', to: 'tasks#show', as: :task
-  patch 'tasks/:id', to: 'tasks#update'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
   # resources :tasks
 end
